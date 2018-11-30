c04853093d3011a4782766430a8cf2b64727be87
zhufu <zhufui@163.com>
2018-11-30 11:16:06 +0800

    增加图片压缩,获取图片信息

--- library/src/main/java/com/zf/library/ImageUtil.java
+++ library/src/main/java/com/zf/library/ImageUtil.java
@@ -2,6 +2,7 @@ package com.zf.library;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
@@ -14,12 +15,211 @@ import android.renderscript.Element;
 import android.renderscript.RenderScript;
 import android.renderscript.ScriptIntrinsicBlur;
 import android.support.annotation.FloatRange;
+import android.support.annotation.IntRange;
 import android.support.annotation.RequiresApi;
+import android.text.TextUtils;
+
+import java.io.ByteArrayOutputStream;
 
 /**
  * 图片工具类
+ * 图片的压缩分三种方式：
+ * 1.质量压缩，src.compress(Bitmap.CompressFormat.JPEG, quality, baos);这里的quality代表质量的取值范围，取值在0-100之间
+ * 2.缩放压缩，即根据宽高对图片进行缩放压缩
+ * 3.采样率压缩,根据设置的宽高和图片的宽高,算出options.inSampleSize的比例
+ * <p>
+ * 一般图片加载到内存中太大了，而我们只想知道那张图片的附件信息也就是宽和高，可以通过将参数options.inJustDecodeBounds设置为true
+ * 就不会将图片加载到内存中了
+ * <p>
+ * bitmapPool,主要是将Bitmap的内存复用，减小内存的开销
+ * inBitmap是可以实现内存复用
+ * options.inBitmap = currentBitmap;
+ * currentBitmap = BitmapFactory.decodeFile(filePath, options);
+ * inBitmap是在安卓3.0出的，3.0以前的手机不能使用
+ * sdk11-18之间，重用的bitmap大小必须一致的，例如给inBitmap赋值的图片大小为100-100，那么新申请的bitmap必须也为 100-100才能够被重用。
+ * 从SDK 19开始，新申请的bitmap大小必须小于或者等于已经赋值过的bitmap大小。
+ * 新申请的bitmap与旧的bitmap必须有相同的解码格式，例如大家都是8888的，如果前面的bitmap是8888，那么就不能支 持4444与565格式的bitmap了，
+ * 不过可以通过创建一个包含多种典型可重用bitmap的对象池，这样后续的bitmap创建都能够找到合适的 “模板”去进行重用。
+ * glide的bitmapPool中就有以下几种编码的bitmap：ALPHA_8,ARGB_4444,ARGB_8888,HARDWARE,RGBA_F16,RGB_565
  */
 public class ImageUtil {
+
+    /**
+     * 根据采样率压缩图片
+     *
+     * @param src       bitmap
+     * @param maxWidth
+     * @param maxHeight
+     * @return
+     */
+    public static Bitmap compressBySampleSize(final Bitmap src,
+                                              final int maxWidth,
+                                              final int maxHeight) {
+        return compressBySampleSize(src, maxWidth, maxHeight, false);
+    }
+
+    public static Bitmap compressBySampleSize(final Bitmap src,
+                                              final int maxWidth,
+                                              final int maxHeight,
+                                              final boolean recycle) {
+        if (isEmptyBitmap(src)) return null;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        src.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] bytes = baos.toByteArray();
+        BitmapFactory.decodeByteArray(bytes, 0, bytes.length, options);
+        options.inSampleSize = calculateInSampleSize(options, maxWidth, maxHeight);
+        options.inJustDecodeBounds = false;
+        if (recycle && !src.isRecycled()) src.recycle();
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.length, options);
+    }
+
+    /**
+     * 根据采样率压缩图片
+     *
+     * @param bytes     字节数组
+     * @param maxWidth
+     * @param maxHeight
+     * @return
+     */
+    public static Bitmap compressBySampleSize(final byte[] bytes,
+                                              final int maxWidth,
+                                              final int maxHeight) {
+        if (bytes == null || bytes.length == 0) return null;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeByteArray(bytes, 0, bytes.length, options);
+        options.inSampleSize = calculateInSampleSize(options, maxWidth, maxHeight);
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.length, options);
+    }
+
+    /**
+     * 根据采样率压缩图片
+     *
+     * @param filePath  文件路径
+     * @param maxWidth
+     * @param maxHeight
+     * @return
+     */
+    public static Bitmap compressBySampleSize(final String filePath,
+                                              final int maxWidth,
+                                              final int maxHeight) {
+        if (TextUtils.isEmpty(filePath)) return null;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;//仅仅解析文件的附件信息
+        BitmapFactory.decodeFile(filePath, options);
+        options.inSampleSize = calculateInSampleSize(options, maxWidth, maxHeight);//通过计算得到采样率,也就是缩放比例
+        options.inJustDecodeBounds = false;//不仅仅解析文件的附件信息,而是加载整个bitmap
+        return BitmapFactory.decodeFile(filePath, options);
+    }
+
+    /**
+     * 根据设置的宽、高和图片本身的宽、高，计算采样率，也就是缩放比例
+     *
+     * @param options
+     * @param maxWidth  设置的宽
+     * @param maxHeight 设置的高
+     * @return
+     */
+    private static int calculateInSampleSize(final BitmapFactory.Options options,
+                                             final int maxWidth,
+                                             final int maxHeight) {
+        int height = options.outHeight;
+        int width = options.outWidth;
+        int inSampleSize = 1;
+        while ((width >>= 1) >= maxWidth && (height >>= 1) >= maxHeight) {
+            inSampleSize <<= 1;
+        }
+        return inSampleSize;
+    }
+
+    /**
+     * 按照质量压缩图片
+     *
+     * @param src
+     * @param quality 指定Bitmap的压缩品质，范围是0 ~ 100；该值越高，画质越高。0表示画质最差，100画质最高。
+     * @return
+     */
+    public static Bitmap compressByQuality(final Bitmap src,
+                                           @IntRange(from = 0, to = 100) final int quality) {
+        return compressByQuality(src, quality, false);
+    }
+
+    public static Bitmap compressByQuality(final Bitmap src,
+                                           @IntRange(from = 0, to = 100) final int quality,
+                                           final boolean recycle) {
+        if (isEmptyBitmap(src)) return null;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        src.compress(Bitmap.CompressFormat.JPEG, quality, baos);
+        byte[] bytes = baos.toByteArray();
+        if (recycle && !src.isRecycled()) src.recycle();
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+    }
+
+    /**
+     * 按照质量压缩图片
+     *
+     * @param src
+     * @param maxByteSize 压缩的最大的byte长度
+     * @return
+     */
+    public static Bitmap compressByQuality(final Bitmap src, final long maxByteSize) {
+        return compressByQuality(src, maxByteSize, false);
+    }
+
+    public static Bitmap compressByQuality(final Bitmap src,
+                                           final long maxByteSize,
+                                           final boolean recycle) {
+        if (isEmptyBitmap(src) || maxByteSize <= 0) return null;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        src.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] bytes;
+        if (baos.size() <= maxByteSize) {
+            bytes = baos.toByteArray();
+        } else {
+            baos.reset();
+            src.compress(Bitmap.CompressFormat.JPEG, 0, baos);
+            if (baos.size() >= maxByteSize) {
+                bytes = baos.toByteArray();
+            } else {
+                // find the best quality using binary search
+                int st = 0;
+                int end = 100;
+                int mid = 0;
+                while (st < end) {
+                    mid = (st + end) / 2;
+                    baos.reset();
+                    src.compress(Bitmap.CompressFormat.JPEG, mid, baos);
+                    int len = baos.size();
+                    if (len == maxByteSize) {
+                        break;
+                    } else if (len > maxByteSize) {
+                        end = mid - 1;
+                    } else {
+                        st = mid + 1;
+                    }
+                }
+                if (end == mid - 1) {
+                    baos.reset();
+                    src.compress(Bitmap.CompressFormat.JPEG, st, baos);
+                }
+                bytes = baos.toByteArray();
+            }
+        }
+        if (recycle && !src.isRecycled()) src.recycle();
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+    }
+
+    /**
+     * 根据宽高缩放压缩图片
+     *
+     * @param src
+     * @param newWidth
+     * @param newHeight
+     * @return
+     */
     public static Bitmap compressByScale(final Bitmap src,
                                          final int newWidth,
                                          final int newHeight) {
@@ -414,5 +614,49 @@ public class ImageUtil {
 
     private static boolean isEmptyBitmap(final Bitmap src) {
         return src == null || src.getWidth() == 0 || src.getHeight() == 0;
+    }
+
+    /**
+     * 获取图片的附加信息
+     *
+     * @param context
+     * @param imageResId 图片的资源id
+     * @return imageInfo, 宽、高、mimeType
+     */
+    public static ImageInfo getImageInfo(Context context, int imageResId) {
+        ImageInfo imageInfo = new ImageInfo();
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;//设置为true,只去读图片的附加信息,不去加载真实的Bitmap
+        BitmapFactory.decodeResource(context.getResources(), imageResId, options);
+        imageInfo.width = options.outWidth;
+        imageInfo.height = options.outHeight;
+        imageInfo.mimeType = options.outMimeType;
+        return imageInfo;
+    }
+
+    /**
+     * 获取图片的附加信息
+     *
+     * @param filePath 文件路径
+     * @return
+     */
+    public static ImageInfo getImageInfo(String filePath) {
+        ImageInfo imageInfo = new ImageInfo();
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;//设置为true,只去读图片的附加信息,不去加载真实的Bitmap
+        BitmapFactory.decodeFile(filePath, options);
+        imageInfo.width = options.outWidth;
+        imageInfo.height = options.outHeight;
+        imageInfo.mimeType = options.outMimeType;
+        return imageInfo;
+    }
+
+    /**
+     * 图片信息
+     */
+    public static class ImageInfo {
+        public int width;
+        public int height;
+        public String mimeType;
     }
 }
